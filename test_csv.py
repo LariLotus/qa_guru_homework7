f4002362102c7dc706f8ba2388ec26fd7deb684a
Larisa Badmaeva <larilotus12@gmail.com>
2023-04-28 13:08:53 +0300

    add files for hw7

--- test_csv.py
+++ test_csv.py
@@ -1,13 +1,21 @@
 import csv
+import os
 # TODO оформить в тест, добавить ассерты и использовать универсальный путь
-with open('resources/eggs.csv', 'w') as csvfile:
-    csvwriter = csv.writer(csvfile, delimiter=',')
-    csvwriter.writerow(['Anna', 'Pavel', 'Peter'])
-    csvwriter.writerow(['Alex', 'Serj', 'Yana'])
 
-with open('resources/eggs.csv') as csvfile:
-    csvreader = csv.reader(csvfile)
-    for row in csvreader:
-        print(row)
-        bbbbb
- xxx   vv
+PROJECT_ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
+resources = os.path.join(PROJECT_ROOT_PATH, 'resources')
+
+def test_csv():
+    csv_path = os.path.join(resources, 'eggs.csv')
+    with open('resources/eggs.csv', 'w') as csvfile:
+        csvwriter = csv.writer(csvfile, delimiter=',')
+        csvwriter.writerow(['Anna', 'Pavel', 'Peter'])
+        csvwriter.writerow(['Alex', 'Serj', 'Yana'])
+
+    with open(csv_path) as csvfile:
+        csvreader = csv.reader(csvfile)
+        csv_read_list = []
+        for row in csvreader:
+            print(row)
+            csv_read_list.append(row)
+    assert csv_read_list[1] == ['Alex', 'Serj', 'Yana']
